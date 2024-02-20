63334d35a6685cb18c8ec6eb7d5fdc62826ccb30
Njoroge Mwangi <ufalmeart@gmail.com>
2024-02-20 12:28:00 +0300

    updated README

--- components/character-details-content.tsx
+++ components/character-details-content.tsx
@@ -1,5 +1,4 @@
 import createApolloClient from '@/lib/apollo-client'
-import { realtimeDatabase } from '@/lib/firebase-config'
 import { gql } from '@apollo/client'
 import { ref, get as getFirebaseData, push, serverTimestamp } from 'firebase/database'
 import { HeartIcon } from '@heroicons/react/24/solid'
@@ -8,6 +7,9 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 // use apollo-client to get character details from graphql end-point
 const client = createApolloClient()
@@ -21,6 +23,27 @@ export default function CharacterDetailsContent({
     id
 }: {id: string}) {
 
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+        databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    
+    const realtimeDatabase = getDatabase(app)
+  
     // firebase realtime datatabse fererence to character notes
     const notesRef = ref(realtimeDatabase, 'characters/notes/' + id)
 
